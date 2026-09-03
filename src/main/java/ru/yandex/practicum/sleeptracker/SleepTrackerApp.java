7bdcced7303b9b3c26c4e9e62712069a73685957
Anna Gomulina <vip.gomulina@yandex.ru>
2026-09-03 21:07:19 +0300

    Удален лишний импорт.

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.sleeptracker;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
--- src/test/java/ru/yandex/practicum/sleeptracker/MaxSessionTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/MaxSessionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MaxSessionTest {
     @Test
